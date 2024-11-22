7e3da9dd7218c210657a60c6e458d18eed43bcd1
marielouisehanna <marielouisehanna2003@gmail.com>
2024-11-22 19:45:49 +0200

    giving up on this one

--- FAILED_PROJECT_InteractiveStory/Main.hs
+++ FAILED_PROJECT_InteractiveStory/Main.hs
@@ -8,9 +8,15 @@ import System.IO (readFile)
 main :: IO ()
 main = do
   script <- readFile "game.txt"
+  putStrLn "Script loaded. Attempting to parse..."
   case runParser gameParser script of
-    [(scenes, "")] ->
+    [(scenes, "")] -> do
+      putStrLn "Parsing succeeded!"
       case findScene "start" scenes of
         Just startScene -> playScene startScene scenes
         Nothing -> putStrLn "No start scene found!"
-    _ -> putStrLn "Failed to parse the game script."
+    _ -> do
+      putStrLn "Failed to parse the game script."
+      putStrLn "Partial parsing result:"
+      print (runParser gameParser script)
+
