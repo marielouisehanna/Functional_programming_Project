bd78a748ccf05ad0bc5dce8838429263470098c1
Yousseffsaade <152533000+Yousseffsaade@users.noreply.github.com>
2024-12-08 21:15:02 +0200

    youssef 3 Update Main.hs

--- Main.hs
+++ Main.hs
@@ -1,17 +1,16 @@
-{-# LANGUAGE OverloadedStrings #-}
+module Main where
 
-import CardGameParser (parseGameFile, writeGameDataToJson)
-import Text.Parsec (parse)
-import System.Environment (getArgs)
+import CardGameParser
+import qualified Data.ByteString.Lazy as B
+import System.IO
+import Text.Parsec
 
 main :: IO ()
 main = do
-    args <- getArgs
-    case args of
-        [inputFile, outputFile] -> do
-            content <- readFile inputFile
-            case parse parseGameFile inputFile content of
-                Left err -> putStrLn $ "Error parsing game file: " ++ show err
-                Right gameData -> writeGameDataToJson gameData outputFile
-
-        _ -> putStrLn "Usage: CardGameParser <input file> <output file>"
+    putStrLn "Parsing the game.txt file..."
+    input <- readFile "game.txt"
+    case parse gameParser "" input of
+        Left err -> print $ "Error: " ++ show err
+        Right game -> do
+            B.writeFile "game.json" (encode game)
+            putStrLn "Parsing successful! game.json generated."
