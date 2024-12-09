15d2bbed4f83acdc8d7b427476738b922710e656
Yousseffsaade <152533000+Yousseffsaade@users.noreply.github.com>
2024-12-09 23:45:38 +0200

    Update Main.hs

--- Main.hs
+++ Main.hs
@@ -1,16 +1,9 @@
 module Main where
 
-import CardGameParser
-import qualified Data.ByteString.Lazy as B
-import System.IO
-import Text.Parsec
+import CardGameParser (parseGameFile)
 
 main :: IO ()
 main = do
-    putStrLn "Parsing the game.txt file..."
-    input <- readFile "game.txt"
-    case parse gameParser "" input of
-        Left err -> print $ "Error: " ++ show err
-        Right game -> do
-            B.writeFile "game.json" (encode game)
-            putStrLn "Parsing successful! game.json generated."
+    putStrLn "Processing game.txt with CardGameParser..."
+    parseGameFile "game.txt"
+    putStrLn "Done! Check game.json for output."
